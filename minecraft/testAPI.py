53d949bc830b5cae5029286ab518e83cc1f8e59b
guhao6869 <guhao6869@gmail.com>
2017-04-08 23:31:19 +0000

    update

--- minecraft/testAPI.py
+++ minecraft/testAPI.py
@@ -5,6 +5,7 @@ from MinecraftAPI import MinecraftAPI
 def main():
         #make a minecraft object mc
 	mc = MinecraftAPI()
+
 	#get the current player position in x, y, z
 	x, y, z = mc.getPlayerPosition()
 	print(x, y, z)
@@ -12,16 +13,18 @@ def main():
 	#set a block at certain position
 	stone = 1
 	grass = 2
-        tnt = 46
+	tnt = 46
         
         #set block to specific position
 	mc.setBlock(x+1,y,z,stone)
 	mc.setBlock(x+2,y,z,grass)
-        mc.setBlock(x+3,y,z,tnt)
+	mc.setBlock(x+3,y,z,tnt)
 
 	#set player position
 	mc.setPlayerPosition(x+1, y, z)
 
 	#get the position of the last setBlock
-	print(mc.getCursor())
+	x, y, z = mc.getCursor()
+	print(x, y, z)
+
 main()
