1feeef660d8dffa3a3b4017445d666f4147c86b0
ProjectNorn <darkisvoid@gmail.com>
2017-04-08 20:59:56 -0400

    Update client.py
    reads host ip from commandline

--- src/client.py
+++ src/client.py
@@ -2,14 +2,23 @@
 
 from MineCoapClient import CoapClient
 import asyncio
+import sys
 
 def main():
     client = CoapClient()
     #must run
     client.connect()
     #id
-    id_uri = 'coap://localhost/id'
-    m_uri = 'coap://localhost/mine'
+    #id_uri = 'coap://localhost/id'
+    #m_uri = 'coap://localhost/mine'
+    if len(sys.argv) < 2:
+        print('No host specified. Assuming localhost')
+        base_uri = 'coap://localhost/'
+    else:
+        base_uri = 'coap://' + sys.argv[1] + '/'
+    #base_uri = 'coap://localhost/' if len(sys.argv) < 2 else ('coap://' + sys.argv[1] + '/')
+    id_uri = base_uri + 'id'
+    mine_uri = base_uri + 'mine'
     #get the id
     my_id = client.get(id_uri)
     colors = ['','blue','red','yellow']
